5f4a3952385c2797e1cf609fbdd4eafd34222364
nilrathana <nilrathana.tep@gmail.com>
2025-01-23 20:21:51 +0700

    #4 update categories-page

--- js/categories.js
+++ js/categories.js
@@ -389,3 +389,11 @@ function deleteCategory(key) {
         }
     });
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menuToggle');
+    const sidebar = document.getElementById('sidebar');
+
+    menuToggle.addEventListener('click', function () {
+        sidebar.classList.toggle('active');
+    });
+});
